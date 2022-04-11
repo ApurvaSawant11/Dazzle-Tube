26d13f3f58cc0d1d4eeb4e1b3720fc46cb402aba
Apurva Sawant <apurvasawant45@gmail.com>
2022-04-11 22:01:53 +0530

    add : card dropdown

--- src/components/card/videoCard/VideoCard.jsx
+++ src/components/card/videoCard/VideoCard.jsx
@@ -1,17 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import "./videoCard.css";
-import { MoreIcon } from "../../../assets";
+import { MoreIcon, PlaylistIcon, WatchLaterIcon } from "../../../assets";
 
 const VideoCard = ({ video }) => {
   const { _id, title } = video;
+  const [dropdown, setDropdown] = useState(false);
   return (
     <div className="video-card">
       <img className="card-img" src={`https://i.ytimg.com/vi/${_id}/0.jpg`} />
-      <div className="flex-row">
+      <div className="flex-row card-details">
         <div className="card-title pr-0p5">{title}</div>
-        <span>
+        <span onClick={() => setDropdown(!dropdown)}>
           <MoreIcon size={24} />
         </span>
+
+        {dropdown && (
+          <ul className="card-dropdown">
+            <li>
+              <WatchLaterIcon className="mr-0p5" /> Add to watch later
+            </li>
+            <li>
+              <PlaylistIcon className="mr-0p5" /> Add to playlist
+            </li>
+          </ul>
+        )}
       </div>
     </div>
   );
