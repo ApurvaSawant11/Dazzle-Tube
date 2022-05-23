de1a524295085e7b562393a908c1a3fb4b32ef68
Apurva Sawant <apurvasawant45@gmail.com>
2022-05-23 17:06:36 +0530

    add: empty message for pages

--- src/pages/liked/LikedVideos.jsx
+++ src/pages/liked/LikedVideos.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import { ListCard } from "../../components";
 import { useVideo } from "../../context";
 import { useDocumentTitle, useScrollToTop } from "../../hooks";
@@ -7,15 +8,28 @@ const LikedVideos = () => {
   useScrollToTop();
   useDocumentTitle("Liked Videos");
   const { videos } = useVideo();
+  const navigate = useNavigate();
+  const likedVideosList = videos.filter((video) => video.isInLiked);
+  const isLikedListEmpty = likedVideosList.length < 1;
   return (
     <div className="video-container">
       <h5 className="text-center mb-2p5">Liked Videos</h5>
 
-      {videos
-        .filter((video) => video.isInLiked)
-        .map((video) => (
+      {isLikedListEmpty ? (
+        <div className="flex-column-center">
+          <p className="text-lg">Looks like you have not liked any videos</p>
+          <button
+            className="button primary radius-0 mt-1"
+            onClick={() => navigate("/")}
+          >
+            Explore Videos Now
+          </button>
+        </div>
+      ) : (
+        likedVideosList.map((video) => (
           <ListCard key={video._id} video={video} />
-        ))}
+        ))
+      )}
     </div>
   );
 };
--- src/pages/playlist/SinglePlaylist.jsx
+++ src/pages/playlist/SinglePlaylist.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./playlist.css";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { useVideo } from "../../context";
 import { PlaylistVideoCard } from "../../components";
 import { useDocumentTitle } from "../../hooks";
@@ -8,6 +8,7 @@ import { useDocumentTitle } from "../../hooks";
 const SinglePlaylist = () => {
   const { playlistId } = useParams();
   const { playlist } = useVideo();
+  const navigate = useNavigate();
 
   const list = playlist.find((list) => list._id === playlistId);
   useDocumentTitle(list.title);
@@ -17,9 +18,21 @@ const SinglePlaylist = () => {
         {list.title}{" "}
         <span className="text-sm ml-0p5"> ({list.videos.length})</span>
       </h4>
-      {list.videos.map((video) => (
-        <PlaylistVideoCard key={video._id} video={video} />
-      ))}
+      {list.videos.length > 0 ? (
+        list.videos.map((video) => (
+          <PlaylistVideoCard key={video._id} video={video} />
+        ))
+      ) : (
+        <div className="flex-column-center">
+          <p className="text-lg px-1">Looks like your playlist is empty</p>
+          <button
+            className="button primary radius-0 mt-1"
+            onClick={() => navigate("/")}
+          >
+            Start Adding Now
+          </button>
+        </div>
+      )}
     </div>
   );
 };
